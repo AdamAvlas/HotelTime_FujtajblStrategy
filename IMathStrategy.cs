bfed33e3a86d4635a965877f7fbcabf7a5b73b5c
adamavlas <adam@avlas.cz>
2023-11-22 17:00:26 +0100

    Add project files.

---
+++ IMathStrategy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FujtajblStrategy
+{
+	public interface IMathStrategy
+	{
+		string Name { get; }
+
+		public int Calculate(int[] values);
+	}
+	public class Addition : IMathStrategy
+	{
+		string IMathStrategy.Name { get { return "Sčítání [a + b]"; } }
+
+		public int Calculate(int[] values)
+		{
+			int result = 0;
+			foreach (int value in values)
+			{
+				result += value;
+			}
+			return result;
+		}
+	}
+	public class Subtraction : IMathStrategy
+	{
+		string IMathStrategy.Name { get { return "Odčítání [a - b]"; } }
+		public int Calculate(int[] values)
+		{
+			int result = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+				result -= values[i];
+            }
+            return result;
+		}
+	}
+
+	public class Multiplication : IMathStrategy
+	{
+		string IMathStrategy.Name { get { return "Násobení [a * b]"; } }
+		public int Calculate(int[] values)
+		{
+			int result = values[0];
+			for (int i = 1; i < values.Length; i++)
+			{
+				result = result * values[i];
+			}
+			return result;
+		}
+	}
+	public class Division : IMathStrategy
+	{
+		string IMathStrategy.Name { get { return "Dělení [a / b]"; } }
+		public int Calculate(int[] values)
+		{
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] == 0)
+                {
+					Console.WriteLine("Nelze dělit nulou! Vyberte jinou operaci.");
+					return 0;
+                }
+            }
+            int result = values[0];
+			for (int i = 1; i < values.Length; i++)
+			{
+				result = result / values[i];
+			}
+			return result;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿namespace FujtajblStrategy
+{
+	internal class Program
+	{
+		static int InputCheck(string varName)
+		{
+			while (true)
+			{
+				Console.Write("Zadejte číslo {0}: ", varName);
+				string input = Console.ReadLine();
+
+				if (!String.IsNullOrEmpty(input))
+				{
+					if (Int32.TryParse(input, out int returnValue))
+					{
+						return returnValue;
+					}
+					else
+					{
+						Console.WriteLine("Zadána neplatná hodnota! Zkuste to prosím znovu.");
+					}
+				}
+				else
+				{
+					Console.WriteLine("Nebyla zadána hodnota! Zkuste to prosím znovu.");
+				}
+			}
+		}
+		public static bool ShouldContinueFunction()
+		{
+			while (true)
+			{
+				string repeat = Console.ReadLine();
+
+				if (repeat == "Y" || repeat == "y")
+				{
+					return true;
+				}
+				if (repeat == "N" || repeat == "n")
+				{
+					Console.WriteLine("Ukončování programu...");
+					return false;
+				}
+				else
+				{
+					Console.WriteLine("Neplatná hodnota. Zkuste znovu.");
+				}
+
+			}
+		}
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.Clear();
+
+				int valueA = InputCheck("A");
+
+				int valueB = InputCheck("B");
+
+				int[] vals = { valueA, valueB };
+				Console.Write("Vaše čísla: " + string.Join("; ", vals) + "\n");
+
+                CalculatorContext context = new CalculatorContext(vals);
+
+				Dictionary<string, IMathStrategy> strategies = new Dictionary<string, IMathStrategy>()
+				{
+					{ "1", new Addition() },
+					{ "2", new Subtraction() },
+					{ "3", new Multiplication() },
+					{ "4", new Division() }
+				};
+
+				Console.WriteLine("Vyberte matematickou operaci: ");
+				foreach (var item in strategies)
+				{
+					Console.WriteLine("[{0}] - {1}", item.Key, item.Value.Name);
+				}
+
+				int result;
+				while (true)
+				{
+					Console.Write("Výběr: ");
+					string operationChoice = Console.ReadLine();
+
+					try
+					{
+						context.SetStrategy(strategies[operationChoice]);
+						result = context.returnValues();
+						break;
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine("Neplatná/prázdná volba! Zkuste znovu. (" + e.Message + ")");
+					}
+
+				}
+
+
+				Console.WriteLine("Výsledek: " + result);
+
+				Console.Write("Přejete si provést další výpočet? [Y/N]: ");
+
+				if (!ShouldContinueFunction()) break;
+			}
+		}
+	}
+
+	public class CalculatorContext
+	{
+		IMathStrategy strategy;
+		int[] values;
+
+		public CalculatorContext(int[] input)
+		{
+			this.values = input;
+		}
+
+		public void SetStrategy(IMathStrategy strategy)
+		{
+			this.strategy = strategy;
+		}
+
+		public int returnValues()
+		{
+			return strategy.Calculate(values);
+		}
+	}
+}
